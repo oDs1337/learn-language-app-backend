bfc19e97793bc425098432fb8b0efdf4f8b29790
oDs1337 <odstobiaszcod@gmail.com>
2023-09-21 14:08:18 +0200

    add models and structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod models;
+mod repository;
+
+#[macro_use]
+extern crate rocket;
+use rocket::{get, http::Status, serde::json::Json};
+
+#[get("/")]
+fn hello() -> Result<Json<String>, Status> {
+    Ok(Json(String::from("Hello, world!")))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello])
 }
---
+++ src/models/swedish_word_model.rs
@@ -0,0 +1,11 @@
+use mongodb::bson::oid::ObjectId;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SwedishWord {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<ObjectId>,
+    pub word: String,
+    pub word_plural: String,
+    pub picture_url: String,
+}
